34649aa8c8fe02e9ea4d826c88dcc31ededabe69
Sudharshan <gks.pani@gmail.com>
2021-05-16 19:53:37 +0530

    Added the name aleart functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("btn");
+let name1 = document.getElementById("input");
+
+button.addEventListener("click",()=>{
+    let input = name1.value;
+    alert(`hello ${input} welcome to learn git`);
+})
